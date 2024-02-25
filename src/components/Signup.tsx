6e8c5b8985308dfd15df5b6cfadd67b711592a57
A Arnob <aliulislamarnob@gmail.com>
2024-02-25 12:12:42 +0600

    SignUp page intro

---
+++ src/components/Signup.tsx
@@ -0,0 +1,71 @@
+// MyForm.js
+import React from 'react';
+import { Form, Input, Button } from 'antd';
+
+
+const formItemLayout = {
+    labelCol: {
+        xs: { span: 24 },
+        sm: { span: 6 },
+    },
+    wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 14 },
+    },
+};
+
+const SignUp = () => {
+    const onFinish = (values: any) => {
+        console.log('Form values:', values);
+        // Handle form submission here
+    };
+
+    return (
+        <Form {...formItemLayout} variant="filled" style={{ maxWidth: 600, margin: "0 auto" }} onFinish={onFinish}>
+            <Form.Item
+                name="username"
+                label="Username"
+                rules={[
+                    { required: true, message: 'Please enter your username' },
+                    // Add more validation rules as needed
+                ]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                name="email"
+                label="email"
+                rules={[
+                    { required: true, message: 'Please enter your email' },
+                    {
+                        type: 'email',
+                        message: 'The input is not valid E-mail!',
+                    },
+                    // Add more validation rules as needed
+                ]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                name="password"
+                label="Password"
+                rules={[
+                    { required: true, message: 'Please enter your password' },
+                    // Add more validation rules as needed
+                ]}
+            >
+                <Input.Password />
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+            </Form.Item>
+        </Form>
+    );
+};
+
+export default SignUp;
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import FileUpload from "./components/FileUpload.tsx";
+import SignUp from "./components/Signup.tsx";
+import SignIn from "./components/SignIn.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      {/* <App /> */}
+      <SignUp />
     </BrowserRouter>
   </React.StrictMode>
 );
